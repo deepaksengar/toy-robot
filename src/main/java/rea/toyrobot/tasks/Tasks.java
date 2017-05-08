ff9a20cc9ec2e8326941b92932e839c1fcb29937
deepak <dsengar3@gmail.com>
2017-05-08 21:42:46 +1000

    removing unused code

--- src/main/java/rea/toyrobot/tasks/Tasks.java
+++ src/main/java/rea/toyrobot/tasks/Tasks.java
@@ -1,27 +1,6 @@
 package rea.toyrobot.tasks;
 
-import rea.toyrobot.errorutility.IllegalTaskArgument;
-import rea.toyrobot.model.position.Position;
-
 public enum Tasks {
 	PLACE, MOVE, LEFT, RIGHT, REPORT;
 
-	public ICommand<Position> executeTask() {
-
-		switch (this) {
-		case PLACE:
-			return new Place();
-		case MOVE:
-			return new Move();
-		case LEFT:
-			return new Left();
-		case RIGHT:
-			return new Right();
-		case REPORT:
-			return new Report();
-
-		default:
-			throw new IllegalTaskArgument("Illegal Command. Could not execute command : " + this.name());
-		}
-	}
 }
